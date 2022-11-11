90d213516f969ee7f9510c6f295663f1fa431856
eloy-freitas <freitaseloy.1@gmail.com>
2022-11-10 21:59:11 -0300

    feat: desenvolvimento Calculo de Bonus por Assiduidade

---
+++ SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/business/calculadora/CalculadoraBonusAssiduidadeStrategy.java
@@ -0,0 +1,50 @@
+package com.ufes.sistemagestaofuncionario.business.calculadora;
+
+import com.ufes.sistemagestaofuncionario.model.Bonus;
+import com.ufes.sistemagestaofuncionario.model.FaltaAoTrabalho;
+import com.ufes.sistemagestaofuncionario.model.Funcionario;
+import java.time.LocalDate;
+
+public class CalculadoraBonusAssiduidadeStrategy extends CalculadoraBonusStrategy{
+
+    public CalculadoraBonusAssiduidadeStrategy() {
+        super("Bonûs por Assiduidade");
+    }
+
+    
+    @Override
+    public Funcionario calcular(Funcionario funcionario) {
+        int quantidade = 0;
+        
+        LocalDate data = LocalDate.now();
+        for(FaltaAoTrabalho fat: funcionario.getFaltaAoTrabalho()){
+            if(fat.getMes().equals(data))
+                quantidade = fat.getQuantidade();
+                break;
+        }
+        
+        Bonus bonus = null;
+        double valor = 0;
+        
+        if(quantidade == 0){
+            valor = 0.05;
+            bonus = new Bonus(this.getNome(), valor, data); 
+        }else if(quantidade >= 1 && quantidade <= 3){
+            valor = 0.03;
+            bonus = new Bonus(this.getNome(), valor, data); 
+        }else if(quantidade >= 4 && quantidade <= 5){
+            valor = 0.01;
+            bonus = new Bonus(this.getNome(), valor, data); 
+        }else{
+            bonus = new Bonus(this.getNome(), valor, data); 
+        }
+        
+        funcionario.getBonusRecebidos().add(bonus);
+        
+        return funcionario;
+
+    }
+    
+    
+    
+}
