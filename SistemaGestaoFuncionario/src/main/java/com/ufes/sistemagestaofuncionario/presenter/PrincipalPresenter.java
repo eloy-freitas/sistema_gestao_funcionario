2c58072ac169f7e49153927c32c6d28e97abdc52
eloy-freitas <freitaseloy.1@gmail.com>
2022-11-12 18:34:48 -0300

    fix: removendo comentário principal presenter

--- SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/presenter/PrincipalPresenter.java
+++ SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/presenter/PrincipalPresenter.java
@@ -1,15 +1,10 @@
 package com.ufes.sistemagestaofuncionario.presenter;
 
-import com.ufes.sistemagestaofuncionario.persistencia.dao.funcionario.FuncionarioDAO;
-import com.ufes.sistemagestaofuncionario.persistencia.repository.funcionario.service.FuncionarioService;
 import com.ufes.sistemagestaofuncionario.view.PrincipalView;
-import java.sql.SQLException;
 
 public class PrincipalPresenter {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //new PrincipalView();
-        
-        FuncionarioService s = new FuncionarioService();
-        s.buscarFuncionarioPorName("abc");
+    public static void main(String[] args) {
+        new PrincipalView();
+
     }
 }
