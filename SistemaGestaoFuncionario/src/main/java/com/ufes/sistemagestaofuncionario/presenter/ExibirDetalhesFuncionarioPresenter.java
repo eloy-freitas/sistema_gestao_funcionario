49f3df7626acf9cbbcf067fcb765fbc0dd28e0f0
Matheus Bastos <mathpbastos@gmail.com>
2022-11-14 12:19:30 -0300

    feat: UI buscar por nome e editar a partir da tabela

--- SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/presenter/ExibirDetalhesFuncionarioPresenter.java
+++ SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/presenter/ExibirDetalhesFuncionarioPresenter.java
@@ -1,14 +1,16 @@
 package com.ufes.sistemagestaofuncionario.presenter;
 
+import com.ufes.sistemagestaofuncionario.model.Cargo;
 import com.ufes.sistemagestaofuncionario.model.Funcionario;
+import com.ufes.sistemagestaofuncionario.persistencia.repository.cargo.service.CargoService;
 import com.ufes.sistemagestaofuncionario.persistencia.repository.funcionario.service.FuncionarioService;
 import com.ufes.sistemagestaofuncionario.view.funcionario.ExibirDetalhesFuncionarioView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.time.format.DateTimeFormatter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
+import java.util.ListIterator;
 import javax.swing.JOptionPane;
 
 public class ExibirDetalhesFuncionarioPresenter {
@@ -16,24 +18,39 @@ public class ExibirDetalhesFuncionarioPresenter {
     private ExibirDetalhesFuncionarioView view;
     private Funcionario funcionario;
     private FuncionarioService funcionarioService;
+    private CargoService cargoService;
+    private List<Cargo> listaCargos;
 
     public ExibirDetalhesFuncionarioPresenter(Funcionario funcionario) {
         view = new ExibirDetalhesFuncionarioView();
         this.funcionario = funcionario;
         desativaCampos();
         initServices();
-        initListeners();
+        initListeners();       
+        populaCargos();
+        initComboBox();
         initCampos(this.funcionario);
         view.setVisible(true);
     }
 
     private void desativaCampos() {
-        view.getTfCargo().setEditable(false);
+        view.getCbCargo().setEditable(false);
         view.getTfNome().setEditable(false);
         view.getFtfSalario().setEditable(false);
         view.getTfFaltas().setEditable(false);
         view.getCheckBoxFuncionarioMes().setEnabled(false);
         view.getTfDataAdmissao().setEditable(false);
+        view.getTfDistanciaTrabalho().setEditable(false);
+    }
+
+    private void ativaCampos() {
+        view.getCbCargo().setEditable(true);
+        view.getTfNome().setEditable(true);
+        view.getFtfSalario().setEditable(true);
+        view.getTfFaltas().setEditable(true);
+        view.getCheckBoxFuncionarioMes().setEnabled(true);
+        view.getTfDataAdmissao().setEditable(false);
+        view.getTfDistanciaTrabalho().setEditable(true);
     }
 
     private void initListeners() {
@@ -65,6 +82,7 @@ public class ExibirDetalhesFuncionarioPresenter {
     private void initServices() {
         try {
             this.funcionarioService = new FuncionarioService();
+            this.cargoService = new CargoService();
         } catch (ClassNotFoundException | SQLException ex) {
             JOptionPane.showMessageDialog(view,
                     "Erro ao carregar serviços necessários."
@@ -75,8 +93,8 @@ public class ExibirDetalhesFuncionarioPresenter {
     }
 
     private void initCampos(Funcionario funcionario) {
-        view.getTfCargo().setText(
-                funcionario.getCargo().getNome());
+        view.getCbCargo().setSelectedItem(
+                this.funcionario.getCargo().getNome());
 
         view.getFtfSalario().setText(Double.toString(
                 funcionario.getSalarioBase()));
@@ -96,12 +114,33 @@ public class ExibirDetalhesFuncionarioPresenter {
                 funcionario.getDataAdmissao()));
     }
 
+    private void populaCargos() {
+        try {
+            listaCargos = cargoService.buscarTodos();
+        } catch (ClassNotFoundException | SQLException ex) {
+            JOptionPane.showMessageDialog(view,
+                    "Ocorreu um erro ao carregar os cargos.\n\n"
+                    + ex.getMessage(),
+                    "ERRO",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void initComboBox() {
+        ListIterator<Cargo> iterator = listaCargos.listIterator();
+
+        while (iterator.hasNext()) {
+            Cargo cargo = iterator.next();
+            view.getCbCargo().addItem(cargo.getNome());
+        }
+        
+    }
+
     private void fechar() {
         view.dispose();
     }
 
     private void excluir(long id) {
-
         try {
             // Mensagem de confirmação
             int opt = JOptionPane.showConfirmDialog(view,
@@ -118,7 +157,8 @@ public class ExibirDetalhesFuncionarioPresenter {
                             "Funcionário excluído com sucesso!",
                             "Sucesso",
                             JOptionPane.INFORMATION_MESSAGE);
-                    view.dispose();
+                    fechar();
+                    new BuscarFuncionarioPresenter();
                 }
             }
         } catch (ClassNotFoundException | SQLException ex) {
@@ -130,7 +170,39 @@ public class ExibirDetalhesFuncionarioPresenter {
         }
     }
 
+    private void obterCampos() {
+        this.funcionario.setNome(view.getTfNome().getText());
+        this.funcionario.setSalarioBase(Double.valueOf(
+                view.getFtfSalario().getText()));
+        this.funcionario.setDistanciaTrabalho(Double.valueOf(
+                view.getTfDistanciaTrabalho().getText()));
+        for(Cargo cargo : listaCargos){
+            if(view.getCbCargo().getSelectedItem().toString()
+                    .equals(cargo.getNome()))
+                this.funcionario.setCargo(cargo);
+        }
+    }
+
     private void editar() {
-        new ManterFuncionarioPresenter();
+        ativaCampos();
+        view.getBtnEditar().setText("Salvar Alterações");
+
+        view.getBtnEditar().addActionListener((ActionEvent e) -> {
+            obterCampos();
+        });
+
+        try {
+            funcionarioService.atualizar(this.funcionario);
+        } catch (ClassNotFoundException | SQLException ex) {
+            JOptionPane.showMessageDialog(view,
+                    "Erro ao atualizar o funcionário.\n\n"
+                    + ex.getMessage(),
+                    "ERRO",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            fechar();
+            new BuscarFuncionarioPresenter();
+        }
+
     }
 }
