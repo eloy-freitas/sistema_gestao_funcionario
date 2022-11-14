77955d146ab8d1e49e03d0d0e6ba35dd0148aefe
Matheus Bastos <mathpbastos@gmail.com>
2022-11-13 21:57:06 -0300

    refactor: change return value do metodo save

--- SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/persistencia/dao/funcionario/IFuncionarioDAO.java
+++ SistemaGestaoFuncionario/src/main/java/com/ufes/sistemagestaofuncionario/persistencia/dao/funcionario/IFuncionarioDAO.java
@@ -7,15 +7,26 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface IFuncionarioDAO {
-    public boolean save(Funcionario funcionario) throws SQLException, ClassNotFoundException;
+
+    public long save(Funcionario funcionario) throws SQLException, ClassNotFoundException;
+
     public boolean save(List<Funcionario> funcionarios) throws SQLException, ClassNotFoundException;
+
     public boolean update(Funcionario funcionario) throws SQLException;
+
     public boolean delete(long id) throws SQLException;
+
     public Funcionario getById(long id) throws SQLException, ClassNotFoundException;
+
     public List<Funcionario> getAll() throws SQLException, ClassNotFoundException;
-    public ResultSet getFuncionarioBonus(long id) throws SQLException, ClassNotFoundException; 
-    public Funcionario getByName(String nome) throws SQLException, ClassNotFoundException; 
-    public List<Funcionario> getAllBuscarFuncionarioView() throws SQLException, ClassNotFoundException; 
-    public ResultSet getSalarioCalculadoByDate(LocalDate data) throws SQLException, ClassNotFoundException; 
-    public ResultSet getAllSalarioCalculado() throws SQLException, ClassNotFoundException; 
+
+    public ResultSet getFuncionarioBonus(long id) throws SQLException, ClassNotFoundException;
+
+    public Funcionario getByName(String nome) throws SQLException, ClassNotFoundException;
+
+    public List<Funcionario> getAllBuscarFuncionarioView() throws SQLException, ClassNotFoundException;
+
+    public ResultSet getSalarioCalculadoByDate(LocalDate data) throws SQLException, ClassNotFoundException;
+
+    public ResultSet getAllSalarioCalculado() throws SQLException, ClassNotFoundException;
 }
